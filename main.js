5b628549d5c7f7b9439ae25c427f8df7649ea36a
Peter <petersrood@gmail.com>
2025-05-24 19:47:32 +0300

    Test #2

--- main.js
+++ main.js
@@ -16,15 +16,15 @@ const footer = document.getElementById("footer");
 
 window.addEventListener("scroll", () => {
   const scrollY = window.scrollY; // Current vertical scroll position starting at 0
-  const footerTop = footer.offsetTop; // Distance to the top position of the footer relative to the document
+  const footerTop = footer.offsetTop; // Distance to the top position of the footer
   const windowHeight = window.innerHeight; // Height of the viewport
 
   //   Show button if scrolled down and not at footer
   //   Checks of 2 conditions:
   //   1. scrolled down more than 300px
   //   2. scrolled down less than the footer's top position minus a buffer
-  //   If both are met, that means the button should be visible.
-  //   Else, it should be hidden.
+  //   If both are met, that means the button should be visible since we are in the middle of the page.
+  //   If not, it means we are either at the top or bottom of the page.
 
   if (scrollY > 300 && scrollY + windowHeight < footerTop) {
     scrollBtn.classList.remove("hidden");
